609f8f32e939430b6a1ea9e93743ae8825b6d9c8
Dusan-Ivic <ivicd99@gmail.com>
2022-06-09 15:22:46 +0200

    Kreiranje socketa ka Sender komponenti

--- replicator/replicator_receiver.py
+++ replicator/replicator_receiver.py
@@ -1,7 +1,27 @@
+import socket
+
+HOST = "127.0.0.1"
+PORT = 65433
 
 class ReplicatorReceiver:
     def __init__(self):
-        pass
+        self.receiver_to_sender_socket = socket.socket()
+
+    def bind_socket(self):
+        try:
+            self.receiver_to_sender_socket.bind((HOST, PORT))
+        except socket.error as e:
+            print(str(e))
+            return False
+        return True
+
+    def start_listening(self):
+        print("Waiting for connections...")
+        self.receiver_to_sender_socket.listen()
+        # TODO - Uspostavljanje konekcije sa Sender komponentom
+
 
 if __name__ == "__main__":
     replicator_receiver = ReplicatorReceiver()
+    if replicator_receiver.bind_socket():
+        replicator_receiver.start_listening()
