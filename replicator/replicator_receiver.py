e02e53971317617d7915f81128da9654aadfa39f
Dusan-Ivic <ivicd99@gmail.com>
2022-06-10 11:57:50 +0200

    Odradjena glavna logika Receiver komponente

--- replicator/replicator_receiver.py
+++ replicator/replicator_receiver.py
@@ -1,4 +1,6 @@
-import socket
+import socket, random
+from data.receiver_property import ReceiverProperty
+from data.collection_description import CollectionDescription
 
 HOST = "127.0.0.1"
 PORT = 65433
@@ -6,6 +8,7 @@ PORT = 65433
 class ReplicatorReceiver:
     def __init__(self):
         self.receiver_to_sender_socket = socket.socket()
+        self.collection_descriptions = {}
 
     def bind_socket(self):
         try:
@@ -15,10 +18,47 @@ class ReplicatorReceiver:
             return False
         return True
 
+    def get_dataset(self, code):
+        if code in ["CODE_ANALOG", "CODE_DIGITAL"]:
+            return 1
+        if code in ["CODE_CUSTOM", "CODE_LIMITSET"]:
+            return 2
+        if code in ["CODE_SINGLENODE", "CODE_MULTIPLENODE"]:
+            return 3
+        if code in ["CODE_CONSUMER", "CODE_SOURCE"]:
+            return 4
+
     def start_listening(self):
         print("Waiting for connections...")
         self.receiver_to_sender_socket.listen()
-        # TODO - Uspostavljanje konekcije sa Sender komponentom
+        connection, address = self.receiver_to_sender_socket.accept()
+        with connection:
+            print(f"Sender connected from: {address[0]}:{address[0]}")
+            while True:
+                try:
+                    data = connection.recv(1024)
+                except ConnectionResetError:
+                    break
+
+                if not data:
+                    break
+                else:
+                    code = data.decode().split(",")[0]
+                    value = data.decode().split(",")[1]
+                    dataset = self.get_dataset(code)
+                    id = random.randint(0, 100)
+                    receiver_property = ReceiverProperty(code, value)
+
+                    if id in self.collection_descriptions:
+                        self.collection_descriptions[id].historical_collection.receiver_properties.append(receiver_property)
+                    else:
+                        collection_description = CollectionDescription(id, dataset)
+                        collection_description.historical_collection.receiver_properties.append(receiver_property)
+                        self.collection_descriptions[id] = collection_description
+
+                    # TODO - Uspostavljanje komunikacije sa Reader komponentom
+
+            connection.close()
 
 
 if __name__ == "__main__":
