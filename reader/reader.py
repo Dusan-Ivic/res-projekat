cf161738165d5bf84289a7954a535f012894fb97
Dusan-Ivic <ivicd99@gmail.com>
2022-06-12 13:51:45 +0200

    Kreiranje Reader komponente i socketa ka Receiveru

--- reader/reader.py
+++ reader/reader.py
@@ -1,7 +1,24 @@
+import socket
+
+HOST = "127.0.0.1"
+STARTING_PORT = 65444
 
 class Reader:
-    def __init__(self):
-        pass
+    def __init__(self, dataset, port):
+        self.dataset = dataset
+        self.port = port
+        self.reader_to_receiver_socket = socket.socket()
+
+    def bind_socket(self):
+        try:
+            self.receiver_to_sender_socket.bind((HOST, self.port))
+        except socket.error as e:
+            print(str(e))
+            return False
+        return True
 
 if __name__ == "__main__":
-    reader = Reader()
+    for i in range(4):
+        reader = Reader(i + 1, STARTING_PORT + i)
+        if reader.bind_socket():
+            pass
