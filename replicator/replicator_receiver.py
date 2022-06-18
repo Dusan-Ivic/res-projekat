110154029ead625ff199278ef19f5a8379478936
Dusan-Ivic <ivicd99@gmail.com>
2022-06-18 22:21:35 +0200

    Ispravljanje gresaka kod slanja delta podataka

--- replicator/replicator_receiver.py
+++ replicator/replicator_receiver.py
@@ -11,7 +11,7 @@ LOG_PORT = 65430
 class ReplicatorReceiver:
     def __init__(self):
         self.receiver_to_sender_socket = socket.socket()
-        self.receiver_to_logger_socker = socket.socket()
+        self.receiver_to_logger_socket = socket.socket()
         self.receiver_to_reader_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.collection_descriptions = {}
         self.delta = DeltaCD()
@@ -26,7 +26,7 @@ class ReplicatorReceiver:
 
     def connect_to_logger(self):
         try:
-            self.receiver_to_logger_socker.connect((HOST, LOG_PORT))
+            self.receiver_to_logger_socket.connect((HOST, LOG_PORT))
         except socket.error as e:
             print(str(e))
             return False
@@ -42,36 +42,54 @@ class ReplicatorReceiver:
         if code in ["CODE_CONSUMER", "CODE_SOURCE"]:
             return 4
 
+    def send_delta_to_reader(self):
+        for id in self.delta.add:
+            for prop in self.delta.add[id].historical_collection.receiver_properties:
+                data = f"{id},{prop.code},{prop.value}"
+                print(data)
+                self.receiver_to_reader_socket.sendto(data.encode(), (HOST, PORT + self.delta.add[id].dataset))
+                log_data = f"[RECEIVER - DELTA ADD],{data},{datetime.now()}"
+                self.receiver_to_logger_socket.send(log_data.encode())
+
+        for id in self.delta.update:
+            for prop in self.delta.update[id].historical_collection.receiver_properties:
+                data = f"{id},{prop.code},{prop.value}"
+                print(data)
+                self.receiver_to_reader_socket.sendto(data.encode(), (HOST, PORT + self.delta.update[id].dataset))
+                log_data = f"[RECEIVER - DELTA UPDATE],{data},{datetime.now()}"
+                self.receiver_to_logger_socket.send(log_data.encode())
+
+        self.delta.clear()
+
     def process_received_data(self, code, value, dataset, id):
         receiver_property = ReceiverProperty(code, value)
 
         if self.delta.ready_to_process():
-            print("SENDING DATA TO READER")
-            for cd in self.delta.add:
-                for prop in cd.historical_collection.receiver_properties:
-                    data = f"{cd.id},{prop.code},{prop.value}"
-                    data_log = f"[RECEIVERADD] {datetime.now()} {data}"
-                    self.receiver_to_reader_socket.sendto(data.encode(), (HOST, PORT + cd.dataset))
-                    self.receiver_to_logger_socker.send(data_log.encode())
-            for cd in self.delta.update:
-                for prop in cd.historical_collection.receiver_properties:
-                    data = f"{cd.id},{prop.code},{prop.value}"
-                    data_log = f"[RECEIVERUPDATE] {datetime.now()} {data}"
-                    self.receiver_to_reader_socket.sendto(data.encode(), (HOST, PORT + cd.dataset))
-                    self.receiver_to_logger_socker.send(data_log.encode())
-            self.delta.clear()
-        elif id in self.collection_descriptions:
-            print("UPDATE")
+            print("Sending Data from Delta to Reader...")
+            self.send_delta_to_reader()
+        
+        if id in self.collection_descriptions:
+            print("Update")
             self.collection_descriptions[id].historical_collection.receiver_properties.append(receiver_property)
             collection_description = CollectionDescription(id, dataset)
             collection_description.historical_collection.receiver_properties.append(receiver_property)
-            self.delta.update.append(collection_description)
+            if id in self.delta.update:
+                self.delta.update[id].historical_collection.receiver_properties.append(receiver_property)
+            else:
+                collection_description = CollectionDescription(id, dataset)
+                collection_description.historical_collection.receiver_properties.append(receiver_property)
+                self.delta.update[id] = collection_description
         else:
-            print("ADD")
+            print("Add")
             collection_description = CollectionDescription(id, dataset)
             collection_description.historical_collection.receiver_properties.append(receiver_property)
             self.collection_descriptions[id] = collection_description
-            self.delta.add.append(collection_description)
+            if id in self.delta.add:
+                self.delta.add[id].historical_collection.receiver_properties.append(receiver_property)
+            else:
+                collection_description = CollectionDescription(id, dataset)
+                collection_description.historical_collection.receiver_properties.append(receiver_property)
+                self.delta.add[id] = collection_description
 
     def start_listening(self):
         print("Waiting for connections...")
