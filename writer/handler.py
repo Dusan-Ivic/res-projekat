b9de7f6347d99c04c0ed570d342b338f69b98d63
Nikola <spacetoast369@gmail.com>
2022-06-21 02:46:23 +0200

    Update handler.py

--- writer/handler.py
+++ writer/handler.py
@@ -22,36 +22,40 @@ while True:
             var = subprocess.Popen('python writer.py', stdout=subprocess.PIPE)
             list_of_writers.insert(writer_counter,var)
             writer_counter += 1
-            print(f"Writer No.{writer_counter+1} is up and running. \n")
+            print(f"Writer No.{writer_counter+1} is up and running.")
         except:
-            print("ERR: Writer failed to open.\n")
+            print("ERR: Writer failed to open.")
 
     elif(user_choice.lower() == 'close'):
         try:
             list_of_writers[writer_counter].terminate()
             writer_counter -= 1
-            print(f"Closing of Writer No.{writer_counter+1} was succesful.\n")
+            print(f"Closing of Writer No.{writer_counter+1} was succesful.")
         except:
-            print("ERR Writer failed to close.\n")
+            print("ERR Writer failed to close.")
 
     elif(user_choice.lower() == 'chose'):
         print("ACTIVE WRITERS")
         print("--------------")
+
         for x in range(writer_counter + 1):
             print(f"Writer {x+1} is active. Shutdown key: {x+1}.")
+
         print("To shutdown Input: Shutdown key num")
         print("To skip Input: x")
+
         user_shutdown_choice = input("Input: ")
         print("\n")
-        if(user_shutdown_choice == 'x' ):
+
+        if(user_shutdown_choice.lower() == 'x'):
             continue
         else:
             try:
                 list_of_writers[int(user_shutdown_choice)-1].terminate()
                 del list_of_writers[int(user_shutdown_choice)-1]
                 writer_counter -= 1
-                print(f"Closing of Writer No.{user_shutdown_choice} was succesful.\n")  
+                print(f"Closing of Writer No.{user_shutdown_choice} was succesful.")  
             except:
-                print("ERR: Writer failed to close or it doesnt exist!\n")    
+                print("ERR: Writer failed to close or it doesnt exist!")    
     else:
         print("ERR: Operation aborted.Incorrect Input.")
